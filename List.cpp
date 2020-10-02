49f0c9e7c6fe40ce481dee9a62cedde7c6fd108a
zef142 <37277962+zef142@users.noreply.github.com>
2020-10-02 16:13:41 -0600

    d

--- List.cpp
+++ List.cpp
@@ -1,1 +1,67 @@
 #include "List.h"
+
+List::List() {
+	start = nullptr;
+	end = nullptr;
+	count == 0;
+};
+
+void List::InsertStart(int value) {
+	nodo* new_nodo = new nodo();
+	new_nodo->value = value;
+
+	if (isEmpty()) {
+		start = new_nodo;
+		end = new_nodo;
+	}
+	else {
+		new_nodo->next = start;
+		start = new_nodo;
+	}
+	count++;
+}
+
+void List::InsertEnd(int value) {
+	nodo* new_nodo = new nodo();
+	new_nodo->value = value;
+
+	if (isEmpty()) {
+		start = new_nodo;
+		end = new_nodo;
+	}
+	else {
+		end->next = new_nodo;
+		end = new_nodo;
+	}
+	count++;
+}
+
+nodo* List::ExtractEnd() {
+	nodo* temp = end;
+	if (!isEmpty()) {
+		if (count == 1) {
+			end = end->next;
+			start = end;
+		}
+		else {
+			nodo* pretemp = start;
+			temp = pretemp->next;
+			while (temp != end) {
+				pretemp = temp;
+				temp = pretemp->next;
+			}
+			pretemp->next = temp->next;
+			end = pretemp;
+		}
+		count--;
+	}
+	return temp;
+}
+
+nodo* List::GetNode(int value) {
+	nodo* temp = start;
+	while ((temp) && (temp->value != value)) {
+		temp = temp->next;
+	}
+	return temp;
+}
